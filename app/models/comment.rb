df1170d0979a2a5b5bd46a319090874b2760a188
openroads <daren22@o2.pl>
2015-06-10 17:24:06 +0000

    wszystko zle

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,5 @@
-class Comment < ActiveRecord::Base
+class Article < ActiveRecord::Base
+  has_many :comments, dependent: :destroy
+  validates :title, presence: true,
+                    length: { minimum: 5 }
 end
--- db/migrate/20150527171612_create_comments.rb
+++ db/migrate/20150527171612_create_comments.rb
@@ -3,9 +3,9 @@ class CreateComments < ActiveRecord::Migration
     create_table :comments do |t|
       t.string :commenter
       t.text :body
-      t.references] :article
+      t.references :article, index: true
 
-      t.timestamps
+      t.timestamps null: false
     end
   end
 end
